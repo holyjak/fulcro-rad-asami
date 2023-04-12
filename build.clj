2c3b66d4b0bf50da28b974af6197885f41434df9
Jakub Holy <jakubholy@jakubholy.net>
2023-04-12 09:25:18 +0200

    Build with pack (WIP)
    I can now run `env DOCKER_HOST=unix:///Users/me/.docker/run/docker.sock pack build rohan-erp-app --builder paketobuildpacks/builder:base`
    
    but it fails at one point with "No such file or directory", possibly due to .dockerignore?

---
+++ build.clj
@@ -0,0 +1,23 @@
+;; Based on https://github.com/borkdude/fly_io_clojure
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'cz.holyjak/fulcro-rad-asami)
+(def version "0.0.1")
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
